9ea64575ff4e2d99c68cdbd04529ebd9901977d7
Zababurin <s.zababurin.v@gmail.com>
2021-10-13 21:49:39 +0300

    add modules

--- frontend/src/modules/eventSource.mjs
+++ frontend/src/modules/eventSource.mjs
@@ -1,4 +1,7 @@
-import isEmpty from "../fs/src/isEmpty/isEmpty.mjs";
+
+// let init = () => {
+//     let eventSource = new EventSource(`${Peer.location.signal}/${Peer.id}?recipient=${Peer.recipient}&initiator=${Peer.initiator}`);
+// }
 
 async function signalServer() {
     return new Promise( async (resolve, reject) => {
@@ -67,52 +70,6 @@ async function signalServer() {
                 success: false,
                 status: false
             })
-        } catch (e) {
-            resolve({
-                message: e.message,
-                success: true,
-                status: true
-            })
-        }
-    })
-}
-// let init = () => {
-//     let eventSource = new EventSource(`${Peer.location.signal}/${Peer.id}?recipient=${Peer.recipient}&initiator=${Peer.initiator}`);
-// }
-
-export default () => {
-    return new Promise(async (resolve, reject) => {
-        try {
-            let object = {}
-            object.params = {}
-            object.params.recipient = false
-            object.params.initiator = false
-            object.params.location = false
-            object.params.id = false
-
-            resolve(new Proxy(  object,{
-                get: (obj, prop) => {
-                    console.log({
-                        _:'get target',
-                        prop:prop,
-                        obj:obj,
-                        value:obj[prop]
-                    })
-                    return obj[prop];
-                },
-                set: (obj, prop, value) => {
-                    console.log({
-                        _:'set target',
-                        prop:prop,
-                        obj:obj,
-                        value:value
-                    })
-                    if(isEmpty(obj[prop])){
-                        obj[prop] = []
-                    }
-                    obj[prop] = value;
-                    return true
-                }}))
         } catch (e) {
             resolve({
                 message: e.message,
