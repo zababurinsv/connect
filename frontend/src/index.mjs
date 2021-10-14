22c3ade704356697cf88cd17334fa0fcfd0306d0
Zababurin <s.zababurin.v@gmail.com>
2021-10-14 20:46:40 +0300

    init

--- frontend/src/index.mjs
+++ frontend/src/index.mjs
@@ -1,31 +1,45 @@
+import api from './modules/api/api.mjs'
 import isEmpty from "./modules/isEmpty/isEmpty.mjs";
-export default () => {
+export default (debug = true, peer = '',initiator = undefined) => {
     return new Promise(async (resolve, reject) => {
         try {
-            let object = {}
-            object.params = {}
+            let object = { }
+            object.params = { }
+            object.api = { }
+            object.params.debug = debug
             object.params.recipient = false
             object.params.initiator = false
             object.params.location = false
             object.params.id = false
-
-            resolve(new Proxy(  object,{
+            object.api = await api(object)
+            resolve(new Proxy(object.api,{
                 get: (obj, prop) => {
-                    console.log({
-                        _:'get target',
-                        prop:prop,
-                        obj:obj,
-                        value:obj[prop]
-                    })
+                    switch (prop) {
+                        case 'params':
+                            return object[prop];
+                            break
+                        default:
+                            break
+                    }
+                    if(object.params.debug) {
+                        console.log({
+                            _:'proxy get',
+                            prop:prop,
+                            obj:obj,
+                            value:obj[prop]
+                        })
+                    }
                     return obj[prop];
                 },
                 set: (obj, prop, value) => {
-                    console.log({
-                        _:'set target',
-                        prop:prop,
-                        obj:obj,
-                        value:value
-                    })
+                    if(object.params.debug) {
+                        console.log({
+                            _:'proxy set',
+                            prop:prop,
+                            obj:obj,
+                            value:value
+                        })
+                    }
                     if(isEmpty(obj[prop])){
                         obj[prop] = []
                     }
---
+++ frontend/src/modules/store/index.mjs
@@ -0,0 +1,5 @@
+import { createStore } from 'redux'
+import index from './default/index.mjs'
+let store = createStore(index)
+
+export default store
